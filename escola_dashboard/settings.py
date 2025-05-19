5fc4796ad79de6b92aabe3d12f28907c23b7da44
Diogo <digo.pasi@gmail.com>
2025-05-19 13:47:01 -0300

    .env

--- escola_dashboard/settings.py
+++ escola_dashboard/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 from datetime import timedelta
 from django.conf import settings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-l3+&hd1z^0g5n4uno4mf1z2#@qmzqkvu(ot106bnlgh_6pwmn3')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
