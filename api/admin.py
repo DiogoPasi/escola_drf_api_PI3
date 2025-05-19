10e82186b0e0e8daa8fe6ce846fa23d141778cd6
Diogo <digo.pasi@gmail.com>
2025-05-19 11:44:33 -0300

    pagina admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import (
+    Administracao,
+    Professores,
+    Responsaveis,
+    Alunos,
+    Materias,
+    Classes,
+    Avaliacoes,
+    Notas
+)
+
+# Link profile > User em Admin
+class AdministracaoInline(admin.StackedInline):
+    model = Administracao
+    can_delete = False
+    verbose_name_plural = 'administracao profile'
+
+class ProfessoresInline(admin.StackedInline):
+    model = Professores
+    can_delete = False
+    verbose_name_plural = 'professor profile'
+
+class AlunosInline(admin.StackedInline):
+    model = Alunos
+    can_delete = False
+    verbose_name_plural = 'aluno profile'
+
+class ResponsaveisInline(admin.StackedInline):
+    model = Responsaveis
+    can_delete = False
+    verbose_name_plural = 'responsavel profile'
+
+
+# Extend the default User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (AdministracaoInline, ProfessoresInline, AlunosInline, ResponsaveisInline)
+
+# Re-register User admin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+# Models
+admin.site.register(Materias)
+admin.site.register(Classes)
+admin.site.register(Avaliacoes)
+admin.site.register(Notas)
+# Ocultar se administrar somente via User admin
+admin.site.register(Administracao)
+admin.site.register(Professores)
+admin.site.register(Responsaveis)
+admin.site.register(Alunos)
