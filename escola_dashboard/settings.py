328067275b331b6a0c1041fd62acca5563854767
Diogo <digo.pasi@gmail.com>
2025-05-19 15:26:38 -0300

    hosts

--- escola_dashboard/settings.py
+++ escola_dashboard/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = [host for host in ALLOWED_HOSTS if host]
 
 
 # Application definition
