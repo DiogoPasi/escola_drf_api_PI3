c3e5383be567d731603cd466878ed77a18955ead
Diogo <digo.pasi@gmail.com>
2025-05-19 11:09:53 -0300

    router e urls api

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
 
+# Router e registro Viewset
+router = DefaultRouter()
+router.register(r'administracao', views.AdministracaoViewSet)
+router.register(r'professores', views.ProfessoresViewSet)
+router.register(r'responsaveis', views.ResponsaveisViewSet)
+router.register(r'alunos', views.AlunosViewSet)
+router.register(r'materias', views.MateriasViewSet)
+router.register(r'classes', views.ClassesViewSet)
+router.register(r'avaliacoes', views.AvaliacoesViewSet)
+router.register(r'notas', views.NotasViewSet)
+
+# API URLs automáticas pelo Router.
 urlpatterns = [
-    # Define your API endpoints here later
-    # path('students/', views.StudentListCreate.as_view(), name='student-list'),
+    path('', include(router.urls)),
 ]
